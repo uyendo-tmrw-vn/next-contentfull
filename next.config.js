3ac0c857ff434c0a4a44b5ae30032d3e9f401b44
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-02-09 15:11:45 +0700

    adsf

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY,
+  },
+  exportPathMap: function () {
+    return {
+      '/': { page: `/` },
+      '/product': { page: `/product` },
+    }
+  },
 }
 
 module.exports = nextConfig
