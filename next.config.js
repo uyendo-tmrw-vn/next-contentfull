999d0a00e3c26f723adba8d6fe2511b328832882
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2023-02-09 14:15:26 +0700

    change env

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_KEY: process.env.CONTENTFUL_ACCESS_KEY,
-  }
 }
 
 module.exports = nextConfig
